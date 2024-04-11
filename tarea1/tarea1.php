6d67a0e749a2b8f562659446bde3e354799d244e
Dailyn Rodriguez <160201485+Dailyn-Rodriguez@users.noreply.github.com>
2024-04-10 18:52:53 -0600

    Add files via upload
    Aqui le envio mi tarea 1 de tecnico de 2 bimestre , 5to baco, Dailyn Marisol Rodriguez Tiu

---
+++ tarea1/tarea1.php
@@ -0,0 +1,6 @@
+<?php 
+echo"Nombre; Dailyn Marisol Rodriguez Tiu"
+    echo"Hola profe ¿como esta? espero que bien ya se me olvido como 
+poner imagen en esto pero aqui le mando para que vea que si funciona mi programa
+saludos ";
+?> 
